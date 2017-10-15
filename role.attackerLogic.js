881293cca814724e16d57debb276d01bfabe247d
DmitryMurinov <murinov.dmitry@yandex.ru>
2017-10-15 09:30:58 +0300

    Допиливание логики комнат.

--- role.attackerLogic.js
+++ role.attackerLogic.js
@@ -49,7 +49,8 @@ module.exports = {
             if (enemies == undefined) {
                 enemies = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS, {
                     filter:
-                        (c) => c.owner.username != 'Source Keeper'
+                        (c) => c.owner.username != 'Source Keeper' &&
+                            c.owner.username != 'Creepz'
                 });
             }
 
