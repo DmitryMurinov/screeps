0e659dca76a71cc5df4fe8f33fa0562c873929ad
Dmitry Murinov <murinov.dmitry.acc@gmail.com>
2020-03-08 23:42:28 +0300

    +Upgraders 1>>3.

--- logic.civilian.js
+++ logic.civilian.js
@@ -399,7 +399,7 @@ module.exports = {
             creepsNeeded.set("pureHarvester", 2);
             creepsNeeded.set("pureMiner", 0);
             creepsNeeded.set("courier", 1);
-            creepsNeeded.set("upgrader", 1);
+            creepsNeeded.set("upgrader", 3);
             creepsNeeded.set("basicCreepOuter1", 2);
             creepsNeeded.set("basicCreepOuter2", 2);
             creepsNeeded.set("basicCreepOuter3", 0);
@@ -566,7 +566,7 @@ module.exports = {
         creepsNeeded.set("pureHarvester", 2);
         creepsNeeded.set("pureMiner", localMadePureMinersNeeded);
         creepsNeeded.set("courier", 2);
-        creepsNeeded.set("upgrader", localMadeUpgradersNeeded);
+        creepsNeeded.set("upgrader", 3);
         creepsNeeded.set("basicCreepOuter1", 2);
         creepsNeeded.set("basicCreepOuter2", 2);
         creepsNeeded.set("basicCreepOuter3", 0);
